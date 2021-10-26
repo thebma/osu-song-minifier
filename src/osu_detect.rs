53b66d5fbdfb9b1f950bfda387a7efff4b7b35a3
thebma <boum.dev@protonmail.com>
2021-10-26 12:10:40 +0200

    Tie in error handling, instead of a break-all like .expected()

--- src/osu_detect.rs
+++ src/osu_detect.rs
@@ -1,16 +1,23 @@
 use winreg::enums::{ HKEY_CLASSES_ROOT, KEY_READ};
+use winreg::RegKey;
 
 pub fn where_is_osu() -> Result<String, ()>
 {
     //NOTE: Basically we're fetching the path that Osu! uses to handle .osz/.osu and etc. files.
     let classes_root = winreg::RegKey::predef(HKEY_CLASSES_ROOT);
-    let osu_reg_key = classes_root.open_subkey_with_flags(r#"osu\shell\open\command"#, KEY_READ)
-        .expect("Failed to find register key containing Osu! install path");
+    let osu_reg_key = classes_root.open_subkey_with_flags(r#"osu\shell\open\command"#, KEY_READ);
+    let registry_key: RegKey;
 
-    let value: String = osu_reg_key.get_value("").unwrap();
+    match osu_reg_key {
+        Ok(v) => { registry_key = v },
+        Err(_) => { return Err(()); }
+    }
+
+    let value: String = registry_key.get_value("").unwrap();
     let value_parts: Vec<&str> = value.split(" ").collect();
 
-    if value_parts.len() >= 1 {
+    if value_parts.len() >= 1 
+    {
         let path_string = value_parts[0].replace("\"", "");
         let path_without_file = path_string.replace(r#"\osu!.exe"#, "");
         Ok(path_without_file)
