b64f54b05d587683c045b9f3d86650400fbdd691
thebma <boum.dev@protonmail.com>
2021-10-26 12:46:23 +0200

    Deduplicate the the directory traversal code.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod osu_format;
 mod osu_detect;
 
 use std::fs;
-use std::io;
 use std::path::{Path, PathBuf};
 use std::time::{Instant};
 
@@ -10,9 +9,10 @@ use std::time::{Instant};
     General todo's for this application:
     - Properly handle Option<> / Error handling on IO errors.
       Unwrapping and praying for the best is the current approach, which arguebly is not the most bulletproof :)
+        Whilest generalizing the iteration code, we nuked all the Result<> structures, an even worse idea.
+        Might wanna test this throughougly!
     - Optionally zip the entire cleaned directory for passing around.
-    - Deduplicate the directory indexing.
-    - Do general testing and see how my code breaks.
+        Having this option would be nice if someone is not willing to sacrifice his Songs folder.
 */
 fn main() 
 {
@@ -25,55 +25,62 @@ fn main()
     };
     
     let songs_path: PathBuf = Path::new(&root).join("Songs");
+
     if songs_path.exists() {
-        recurse_songs(songs_path).unwrap()
+        iterate_songs(songs_path);
     }
 
     let time = Instant::now().saturating_duration_since(start).as_secs_f32();
     println!("Execution time: {} seconds", time);
 }
 
-fn recurse_songs(songs_folder: PathBuf) -> Result<(), io::Error>
+fn recurse_directory(path: PathBuf, predicate: impl Fn(PathBuf) -> bool, callback: impl Fn(PathBuf))
 {
-    for entry in fs::read_dir(songs_folder)?
+    for entry in fs::read_dir(path).unwrap()
     {
-        let entry: fs::DirEntry = entry?;
+        let entry: fs::DirEntry = entry.unwrap();
         let path = entry.path();
-
-        if path.is_dir() 
-        {
-            process_song(entry.path())?;
+        
+        if predicate(path.clone()) {
+            callback(path.clone());
         }
     }
-
-    Ok(())
 }
 
-fn process_song(song_path: PathBuf) -> Result<(), io::Error>
+fn iterate_songs(songs_folder: PathBuf)
 {
-    for entry in fs::read_dir(song_path)? 
+    recurse_directory(songs_folder,
+        | path | { path.exists() && path.is_dir() },
+        | path | { evaluate_song(path); }
+    );
+}
+
+fn evaluate_song(song_path: PathBuf)
+{
+    iterate_song_files(song_path);
+}
+
+fn iterate_song_files(song_path: PathBuf)
+{
+    recurse_directory(song_path,
+        | path | { path.exists() && path.is_file() },
+        | path | { evaluate_song_files(path); }
+    );
+}
+
+fn evaluate_song_files(song_file_path: PathBuf)
+{
+    let extension = song_file_path.extension();
+
+    if let Some(ext) = extension 
     {
-        let entry: fs::DirEntry = entry?;
-        let path: PathBuf = entry.path();
+        let ext_str: &str = ext.to_str().unwrap();
 
-        if path.is_file()
+        if ext_str.contains("osu")
         {
-            let extension = path.extension();
-
-            if let Some(ext) = extension 
-            {
-                let ext_str: &str = ext.to_str().unwrap();
-
-                if ext_str.contains("osu")
-                {
-                    //println!("evaluating: {:?}", path);
-                    find_critical_files(path).unwrap();
-                }
-            }
+            find_critical_files(song_file_path);
         }
     }
-
-    Ok(())
 }
 
 ///
@@ -81,7 +88,7 @@ fn process_song(song_path: PathBuf) -> Result<(), io::Error>
 /// osu_path is the .osu file with in a song directory, this contains the metadata what the background and audio files are.
 /// Returns a list of files to keep.
 /// 
-fn find_critical_files(osu_path: PathBuf)  -> Result<Vec<String>, io::Error>
+fn find_critical_files(osu_path: PathBuf)
 {
     let mut files_to_keep: Vec<String> = Vec::new();
     let mut file = osu_format::OsuFile::new();
@@ -90,6 +97,4 @@ fn find_critical_files(osu_path: PathBuf)  -> Result<Vec<String>, io::Error>
     println!("Audio file is {}", file.general_section.audio_file_name);
     println!("Background file is {:?}", file.events_section.background);
     println!("Video file is {:?}", file.events_section.video);
-
-    Ok(files_to_keep)
 }
